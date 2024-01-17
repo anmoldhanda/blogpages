152c8f8a62a962b89069b0435bec41f02235b1c5
anmol dhanda <rk352714@gmail.com>
2024-01-17 14:21:09 +0530

    search bar functionality for searching blogs

--- js/main.js
+++ js/main.js
@@ -10,8 +10,22 @@ hamburgermenu.addEventListener("click", () => {
 // ================================ hide show the search bar ================================
 const searchbloginputfield = document.getElementById("searchbloginputfield");
 const searchblogicon = document.getElementById("searchblogicon");
+const blogcards = document.getElementsByClassName("blogcards");
 searchblogicon.addEventListener("click", () => {
   searchbloginputfield.classList.toggle("hideshowsearchbar");
+});
+
+// search bar functionality if the user's searched blog is available then it will show the specified blog otherwise it will set the container empty
+searchbloginputfield.addEventListener("keyup", () => {
+  let searchterm = searchbloginputfield.value.toLowerCase();
+  let singleblogcard = Array.from(blogcards).forEach((singleblogcard) => {
+    let blogcardtext = singleblogcard.textContent.toLowerCase();
+    if (blogcardtext.includes(searchterm)) {
+      singleblogcard.style.display = "flex";
+    } else {
+      singleblogcard.style.display = "none";
+    }
+  });
 });
 
 // ================================ blog comments form ================================
